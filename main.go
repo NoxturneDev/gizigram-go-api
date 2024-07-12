0bd4293ac3f55cf863a4c87de533e4cb407e19d1
Galih Adhi Kusuma <noxturne.production@gmail.com>
2024-07-12 12:47:00 +0700

    feat: enable cors

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
-	"github.com/berkatps/database"
-	"github.com/berkatps/model"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"gizigram-go-api/database"
+	"gizigram-go-api/model"
 )
 
 func main() {
 	app := fiber.New()
 
-	database.ConnectDatabase()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
+	database.ConnectDatabase()
 	database.DB.AutoMigrate(&model.Users{}, &model.Parent{}, &model.Children{}, &model.GrowthRecord{})
 
 	NewRouter(app)
